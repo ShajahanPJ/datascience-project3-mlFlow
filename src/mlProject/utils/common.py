b8936b56eb7a926f40e526d080e68c2e198d23f8
Shajahan <shajahanpj10@gmail.com>
2024-04-01 12:31:50 +0530

    data ingestion added

--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -16,7 +16,7 @@ def read_yaml(path_to_yaml:Path)->ConfigBox:
         with open (path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded succesfully")
-            return ConfigBox
+            return ConfigBox(content)
     except BoxValueError:
         raise ValueError("Yaml file is empty")
     except Exception as e:
