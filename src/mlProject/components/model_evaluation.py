bbe093cbf75a0478bb7a27c12ef3064cea4d39e7
Shajahan <shajahanpj10@gmail.com>
2024-04-17 20:46:18 +0530

    model evaluation modified

--- src/mlProject/components/model_evaluation.py
+++ src/mlProject/components/model_evaluation.py
@@ -10,13 +10,12 @@ from mlProject.entity.config_entity import ModelEvaluationConfig
 from mlProject.utils.common import save_json
 from pathlib import Path
 
-
 class ModelEvaluation:
     def __init__(self, config: ModelEvaluationConfig):
         self.config = config
 
     
-    def eval_metrics(self,actual, pred):
+    def eval_metrics(self,actual,pred):
         rmse = np.sqrt(mean_squared_error(actual, pred))
         mae = mean_absolute_error(actual, pred)
         r2 = r2_score(actual, pred)
@@ -25,13 +24,11 @@ class ModelEvaluation:
 
 
     def log_into_mlflow(self):
-
         test_data = pd.read_csv(self.config.test_data_path)
         model = joblib.load(self.config.model_path)
 
         test_x = test_data.drop([self.config.target_column], axis=1)
         test_y = test_data[[self.config.target_column]]
-
 
         mlflow.set_registry_uri(self.config.mlflow_uri)
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
@@ -66,3 +63,4 @@ class ModelEvaluation:
                 mlflow.sklearn.log_model(model, "model")
 
     
+
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -67,7 +67,7 @@ class ConfigurationManager:
     def get_model_trainer_cofig(self)-> ModelTrainerConfig:
         config = self.config.model_trainer
         params = self.params.ElasticNet
-        schema = self.schema.TARGETED_COLUMN
+        schema = self.schema.TARGET_COLUMN
 
         create_directories([config.root_dir])
 
@@ -84,22 +84,22 @@ class ConfigurationManager:
         return model_trainer_config
     
 
-    def get_model_evaluation_config(self)-> ModelEvaluationConfig:
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
         config = self.config.model_evaluation
         params = self.params.ElasticNet
-        schema = self.schema.TARGETED_COLUMN
-
+        schema =  self.schema.TARGET_COLUMN
 
         create_directories([config.root_dir])
 
         model_evaluation_config = ModelEvaluationConfig(
-            root_dir  = config.root_dir,
-            test_data_path = config.test_data_path,
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
             model_path = config.model_path,
-            all_params = params,
+            all_params=params,
             metric_file_name = config.metric_file_name,
             target_column = schema.name,
-            mlflow_uri = "https://dagshub.com/ShajahanPJ/datascience-project3-mlFlow.mlflow"
-
+            mlflow_uri="https://dagshub.com/ShajahanPJ/datascience-project3-mlFlow.mlflow",
+           
         )
+
         return model_evaluation_config
